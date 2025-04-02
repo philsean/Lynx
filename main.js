fe4b9bea8b1c02eeb54c655d0255ee3852c06da5
Sean <140343656+philsean@users.noreply.github.com>
2025-04-02 08:54:57 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 $jsonParse[$getVar[blacklist;$authorID]]
-$if[$json[time]>=$getTimestamp]
-$reply
+$if[$getVar[blacklist;$authorID]!={}]
+  $if[$json[time]>=$getTimestamp]
+    $reply
 ( 🛡️ ) › Você foi banido de usar meu sistema, caso ache que seu banimento foi injusto, contate `$username[$botOwnerID]`.
 >>> **Motivo:** `$unescape[$json[reason]]`
 **Tempo:**$try <t:$json[time]:R> $c[$sub[$json[time];1]] $catch `Indeterminado.` $endtry
-$stop
+    $stop
+  $endif
 $endif
 
 $httpGet[$getVar[bot.tr]]
